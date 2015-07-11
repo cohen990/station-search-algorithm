e75637e0872357f6614895a8ca77209223fd4151
dancohen447 <dancohen447@gmail.com>
2015-07-11 16:20:43 +0100

    done - kinda

---
+++ StationSearchAlgorithm/SearchResult.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace StationSearchAlgorithm
+{
+	public class SearchResult
+	{
+		public List<string> Matches { get; private set; }
+
+		public List<char> Suggestions { get; private set; }
+
+		private SearchResult()
+		{
+			Matches = new List<string>();
+			Suggestions = new List<char>();
+		}
+
+		public SearchResult(List<string> matches, List<char> suggestions)
+		{
+			Matches = matches;
+			Suggestions = suggestions;
+		}
+
+		public static SearchResult Empty
+		{
+			get { return new SearchResult(); }
+		}
+	}
+}
